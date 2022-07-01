ab22c940f784ca0c0a94e25d9bd4d59de8ef560a
Denis <denis.tolmachev@gmail.com>
2022-07-01 23:23:53 +0300

    add state in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import {FeedbackBtns} from './components/FeedbackBtns/FeedbackBtns';
-import {Statistics } from './components/Statistics/Statistics';
+import { Component } from 'react';
+import { FeedbackBtns } from './components/FeedbackBtns/FeedbackBtns';
+import { Statistics } from './components/Statistics/Statistics';
 
-export const App = () => {
-  return (
-    <div>
-      <h2>Please leave feedback</h2>
-      <FeedbackBtns />
-      <h2>Statistics</h2>
-      <Statistics />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <FeedbackBtns />
+        <h2>Statistics</h2>
+        <Statistics />
+      </div>
+    );
+  }
+}
